03617ca5b4a00663b3e63d2788946f9f5fe4715a
Rifat <rifat.khaidir@gmail.com>
2025-07-19 00:47:14 +0800

    feat(data): update and add detail information about the destination in card

---
+++ src/data/cardData.js
@@ -0,0 +1,38 @@
+export const cardData = [
+  {
+    place: 'Desa Lainungan',
+    title: 'BUJUNG',
+    title2: 'PITUE',
+    description: 'Gunung Sumur7 menawarkan pemandangan alam yang memukau dengan ketinggian yang menantang. Destinasi ini populer bagi pendaki pemula dan profesional.',
+    image: '/images/bujungpitue.jpg'
+  },
+  {
+    place: 'Air Terjun Macaya',
+    title: 'AIR TERJUN',
+    title2: 'MACAYA',
+    description: 'Air terjun dengan ketinggian 50 meter yang dikelilingi oleh hutan tropis yang lebat. Suasana sejuk dan aliran air yang jernih membuatnya menjadi tempat yang sempurna untuk relaksasi.',
+    image: '/images/kincirhighland.png'
+  },
+  {
+    place: 'Lainungan',
+    title: 'TAMAN',
+    title2: 'NONA-NONA',
+    description: 'Taman rest area Lainungan',
+    image: '/images/nonanona.jpg'
+  },
+  {
+    place: 'Kincir Angin Tradisional',
+    title: 'KINCIR',
+    title2: 'TRADISIONAL',
+    description: 'Kincir angin tradisional yang masih beroperasi untuk mengairi sawah. Merupakan warisan budaya dan teknologi lokal yang patut dilestarikan.',
+    image: '/images/kincir.webp'
+  },
+  {
+    place: 'Bukit Macaya',
+    title: 'Macaya',
+    title2: '',
+    description: 'Bukit di lainungan',
+    image: '/images/macaya.jpg'
+  },
+  // Tambahkan data destinasi lainnya sesuai kebutuhan
+];
