bdf45b44475a0a3a965c06fc0b43a2e335555e00
Rifat <rifat.khaidir@gmail.com>
2025-07-24 09:51:36 +0800

    feat : add firebase dependancy

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,17 @@
 "use client";
 import { useState } from "react";
 import TimedCards from "../components/TimeCards";
+import { db } from '@/firebase/firebase';
+import { collection, addDoc } from 'firebase/firestore';
 
 export default function Home() {
   const [isIntroDone, setIsIntroDone] = useState(false);
+  const handleSubmit = async () => {
+    await addDoc(collection(db, 'users'), {
+      name: 'Java',
+      email: 'rifat.khaidir@gmail.com',
+    });
+  };
 
   // Tampilkan animasi jika belum selesai
 
