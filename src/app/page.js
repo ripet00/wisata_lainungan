13bab4c289a7780623ffce2e43da02d312f117ce
Rifat <rifat.khaidir@gmail.com>
2025-07-25 14:30:07 +0800

    feat : add login link button

--- src/app/page.js
+++ src/app/page.js
@@ -1,25 +1,30 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import TimedCards from "../components/TimeCards";
 import { db } from '@/firebase/firebase';
-import { collection, addDoc } from 'firebase/firestore';
+import { collection, getDocs } from 'firebase/firestore';
+import Link from "next/link";
 
 export default function Home() {
-  const [isIntroDone, setIsIntroDone] = useState(false);
-  const handleSubmit = async () => {
-    await addDoc(collection(db, 'users'), {
-      name: 'Java',
-      email: 'rifat.khaidir@gmail.com',
-    });
-  };
+  const [cards, setCards] = useState([])
 
-  // Tampilkan animasi jika belum selesai
+  useEffect(() => {
+    const fetchCards = async () => {
+      const snapshot = await getDocs(collection(db, 'cards'))
+      const data = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data}))
+      setCards(data)
+    }
+    fetchCards()
+  }, [])
 
-  // Tampilkan konten utama setelah animasi selesai
   return (
     <main>
       <TimedCards/>
-
+      <div className="text-center mt-4">
+        <Link href="/login" className="text-blue-500 underline">
+          Admin Login
+        </Link>
+      </div>
     </main>
   );
 }
