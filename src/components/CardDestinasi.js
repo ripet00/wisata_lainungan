06ad821744544e35f5508ed8e3d6efd9d96cdaf0
Rifat <rifat.khaidir@gmail.com>
2025-08-14 18:46:03 +0800

    feat : fix deployment problem

--- src/components/CardDestinasi.js
+++ src/components/CardDestinasi.js
@@ -1,7 +1,16 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function CardDestinasi({ title, image }) {
   return (
     <div className="rounded shadow hover:shadow-lg transition overflow-hidden bg-white">
-      <img src={image} alt={title} className="w-full h-40 object-cover" />
+      <Image 
+        src={image} 
+        alt={title} 
+        width={400} // sesuaikan
+        height={160} // sesuaikan
+        className="w-full h-40 object-cover"
+      />
       <div className="p-4">
         <h3 className="font-semibold text-lg">{title}</h3>
         <Link href="/destinasi" className="text-blue-500 text-sm">
